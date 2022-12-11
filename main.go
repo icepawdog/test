2fe4be513af00e7388d6caf15d8e8e8a71fed67f
yaroslav_ryzhikh <yaroslav.ryzhikh@sbermarket.ru>
2022-12-11 20:03:31 +0300

    первая версия

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var text string
+	var width int
+	fmt.Scanf("%s %d", &text, &width)
+	runes := []rune(text)
+	res := string(runes[0:width])
+
+	fmt.Printf("%v...", res)
+}
